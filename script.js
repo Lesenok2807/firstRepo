b9dc8e3aab09bd7f869ab95e0897d354d399f96b
Olesia Ivanova <ivanova.ov_87@mail.ru>
2023-08-18 19:09:11 +0300

    добавление переменных

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const title = "first project";
+const screens = 10;
+const screenPrice = 10000;
+let rollback;
+const fullPrice = 25000;
+const adaptive = 25;
+
+alert("Привет! Давай начнем!");
+console.log(screenPrice);
