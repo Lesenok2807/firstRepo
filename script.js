8e50f79478a4cbdaad6027a86effd6bd454b1e9b
Olesia Ivanova <ivanova.ov_87@mail.ru>
2023-08-18 20:37:00 +0300

    присвоение переменным значений

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const title = "first project";
-const screens = 10;
-const screenPrice = 10000;
-let rollback;
-const fullPrice = 25000;
-const adaptive = 25;
+const title = "Название проекта";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 1000;
+let rollback = 88;
+const fullPrice = 125000;
+const adaptive = true;
 
-alert("Привет! Давай начнем!");
-console.log(screenPrice);
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов = " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта = " + fullPrice + " рублей");
+console.log(screens.toLocaleLowerCase().split(", "));
+console.log(fullPrice * (rollback/100));
