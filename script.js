ec7446df69ccafceee178f106655cbb3a936bb9d
Olesia Ivanova <ivanova.ov_87@mail.ru>
2023-08-21 20:10:42 +0300

    коммит к третьему уроку

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const title = "Название проекта";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 1000;
-let rollback = 88;
-const fullPrice = 125000;
-const adaptive = true;
+'use strict';
 
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+const screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
+const rollback = 36;
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?", "1000");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?", "1000");
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePresentPrice = Math.ceil(fullPrice - ((rollback / 100) * fullPrice));
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов = " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта = " + fullPrice + " рублей");
-console.log(screens.toLocaleLowerCase().split(", "));
-console.log(fullPrice * (rollback/100));
+console.log(fullPrice);
+console.log(servicePresentPrice);
+
+switch (true) {
+    case fullPrice >= 30000:
+        console.log('Делаем скидку 10%');
+        break;
+    case 15000 <= fullPrice && fullPrice < 30000:
+        console.log('Делаем скидку 5%');
+        break;
+    case 0 < fullPrice && fullPrice < 15000:
+        console.log('Скидка не предусмотрена');
+        break;
+    default:
+        console.log('Что-то пошло не так');
+}
