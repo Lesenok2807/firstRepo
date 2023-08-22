0e0d0167158ecf1e43e7d17298edffb70487c5ed
Olesia Ivanova <ivanova.ov_87@mail.ru>
2023-08-22 20:52:47 +0300

    первый коммит к 4 уроку

--- script.js
+++ script.js
@@ -3,28 +3,55 @@
 const title = prompt("Как называется ваш проект?");
 const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
 const screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
-const rollback = 36;
+const rollback = 25;
 const adaptive = confirm("Нужен ли адаптив на сайте?");
 const service1 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice1 = +prompt("Сколько это будет стоить?", "1000");
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = +prompt("Сколько это будет стоить?", "1000");
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const allServicePrices = servicePrice1 + servicePrice2;
+const fullPrice = screenPrice + allServicePrices;
 const servicePresentPrice = Math.ceil(fullPrice - ((rollback / 100) * fullPrice));
 
-console.log(fullPrice);
-console.log(servicePresentPrice);
-
-switch (true) {
-    case fullPrice >= 30000:
-        console.log('Делаем скидку 10%');
-        break;
-    case 15000 <= fullPrice && fullPrice < 30000:
-        console.log('Делаем скидку 5%');
-        break;
-    case 0 < fullPrice && fullPrice < 15000:
-        console.log('Скидка не предусмотрена');
-        break;
-    default:
-        console.log('Что-то пошло не так');
+const showTypeOf = function(variable) {
+    console.log(variable, typeof variable);
 }
+
+function getTitle() {
+    let newTitle = title.trim(); 
+    return newTitle[0].toUpperCase() + newTitle.slice(1).toLowerCase();   
+} 
+
+const getAllServicePrices = function() {    
+    return allServicePrices;
+}
+
+function getFullPrice() {
+    return fullPrice;
+}
+
+function getServicePercentPrices() {
+    return servicePresentPrice;
+}
+
+const getRollbackMessage = function(price) {
+    switch (true) {
+        case price >= 30000:
+            return 'Делаем скидку 10%';
+        case 15000 <= price && price < 30000:
+            return 'Делаем скидку 5%';
+        case 0 < price && price < 15000:
+            return 'Скидка не предусмотрена';
+        default:
+            return 'Что-то пошло не так';
+    }
+}
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
+console.log(getTitle());
+console.log(getServicePercentPrices());
+console.log(screens.toLowerCase().split(", "));
